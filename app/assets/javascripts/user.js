b1f3d582a63b0909e463ae8dddfe35ada70fba1c
terakado <sakusaku.sasuke0426@gmail.com>
2019-06-14 14:31:12 +0900

    update

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,22 @@
 $(function() {
+
+  var search_list = $('#user-search-result');
+
+  function buildHtml(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendErrMsgToHTML(msg){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>`
+    search_list.append(html);
+  }
+
   $('#user-search-field').on('keyup', function() {
     var input = $(this).val();
     console.log(input);
@@ -15,12 +33,15 @@ $(function() {
       $('#user-search-result').empty();
       if(users.length !== 0) {
         users.forEach(function(user) {
-          console.log(user.name)
+          buildHtml(user)
         });
+      } else {
+        appendErrMsgToHTML('一致するユーザーはいません');
       }
+
     })
     .fail(function() {
-
+      alert('ユーザー検索に失敗しました')
     })
   })
 });
